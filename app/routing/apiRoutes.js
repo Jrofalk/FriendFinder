88b02bc025a105f52a22c9efe2c664e3d36800c6
John Falk <jrfalk@outlook.com>
2019-08-17 18:57:13 -0400

    Created for loop within post that returns each users score

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,18 +2,19 @@ var friendData = require("../data/friends");
 
 var path = require('path');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res) {
-        res.json(friendData);
-      });
+  app.get("/api/friends", function (req, res) {
+    res.json(friendData);
+  });
 
 
-      app.post("/api/friends", function(req, res) {
-        var newFriend = req.body;
-        
-
-        friendData.push(newFriend);
-        res.json(newFriend);
-      });
+  app.post("/api/friends", function (req, res) {
+    var newFriend = req.body;
+    friendData.push(newFriend);
+    res.json(newFriend);
+    for (let i = 0; i < friendData.length; i++) {
+      console.log(friendData[i].scores);
     }
+  });
+}
