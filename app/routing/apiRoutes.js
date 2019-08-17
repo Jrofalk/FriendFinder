8bc8f8248682f20d6eb6ccd790642d3fe173f5d7
John Falk <jrfalk@outlook.com>
2019-08-17 18:26:23 -0400

    Clicking submit now initiates post which pushes user information into friendsArray

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,7 @@
 var friendData = require("../data/friends");
 
+var path = require('path');
+
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
@@ -8,6 +10,10 @@ module.exports = function(app) {
 
 
       app.post("/api/friends", function(req, res) {
+        var newFriend = req.body;
         
+
+        friendData.push(newFriend);
+        res.json(newFriend);
       });
     }
