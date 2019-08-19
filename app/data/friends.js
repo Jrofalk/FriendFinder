6380740196592a9b070fdfb9b98ccc50e023920d
John Falk <jrfalk@outlook.com>
2019-08-19 19:06:02 -0400

    Added additional friends to friend.js, including images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,79 @@
 var friendsArray = [
     {
-        name:"Ahmed",
+        name:"Cat",
         photo: "https://thumbs-prod.si-cdn.com/d4e3zqOM5KUq8m0m-AFVxuqa5ZM=/800x600/filters:no_upscale():focal(554x699:555x700)/https://public-media.si-cdn.com/filer/a4/04/a404c799-7118-459a-8de4-89e4a44b124f/img_1317.jpg",
         scores:[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
             2,
-            5,
             4,
-            1]
-    }
+            2,
+            3,
+            4,
+            5,
+            2,
+            1,
+            3,
+            3]
+    },
+    {
+            name:"Dog",
+            photo: "https://www.petmd.com/sites/default/files/diarrhea-long-term-dogs.jpg",
+            scores:[
+                5,
+                2,
+                5,
+                4,
+                1,
+                4,
+                3,
+                4,
+                1,
+                5]
+    },
+    {
+        name: "Rock",
+        photo: "https://www.wallingtons.com.au/wp-content/uploads/2014/11/Granite-Rocks-600x391.jpg",
+        scores:[
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1]
+},
+{
+        name: "Lizard",
+        photo: "https://blog.nationalgeographic.org/wp-content/uploads/2013/12/1-one-way-breathing-lizard-s2048x1957-p.jpg",
+        scores:[
+                3,
+                3,
+                3,
+                3,
+                3,
+                3,
+                3,
+                3,
+                3,
+                3]
+},
+{
+        name: "Bird",
+        photo: "https://img-aws.ehowcdn.com/750x428p/photos.demandstudios.com/getty/article/19/45/497894918_XS.jpg",
+        scores:[
+                5,
+                4,
+                3,
+                2,
+                2,
+                3,
+                2,
+                1,
+                1,
+                4]
+}
   ];
 
   module.exports = friendsArray;
